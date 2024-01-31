f42b7876a578aac15a1140665b5959fbda9ef17b
Sefa AŞIK <sefa.a1707@gmail.com>
2024-01-31 22:03:55 +0300

    # Hakkımızda Alanı Listelendi. -Hakkımızda bilgileri index sayfası üzerinde listelendi.

---
+++ MvcCvWebProject/Controllers/VitrinController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvWebProject.Models.Entity;
+
+namespace MvcCvWebProject.Controllers
+{
+    public class VitrinController : Controller
+    {
+        // GET: Vitrin
+        MvcCvDbEntities db=new MvcCvDbEntities();   
+        public ActionResult Index()
+        {
+            var hakkimda = db.TblHakkimda.ToList();
+            return View(hakkimda);
+        }
+    }
+}
