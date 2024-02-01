3da0441d0de323e6f474af6fbd368d0ee4af1821
Sefa AŞIK <sefa.a1707@gmail.com>
2024-02-01 13:46:35 +0300

    #Vitrin Sayfası Listeleme İşlemleri ve Mesaj Gönderme Tamamlandı. -Hakkımda kısmı haricinde bulunan alanları listelenmesi farklı tablolar üzerinden olduğu için her alana özel PartialView oluşturuldu. -Deneyim, Eğitim, Sertifika, Hobi, Yetenek alanları listelemeleri yapıldı. -PartialView üzerinden mesaj gönderme işlemleri sağlandı.

--- MvcCvWebProject/Controllers/VitrinController.cs
+++ MvcCvWebProject/Controllers/VitrinController.cs
@@ -10,11 +10,53 @@ namespace MvcCvWebProject.Controllers
     public class VitrinController : Controller
     {
         // GET: Vitrin
-        MvcCvDbEntities db=new MvcCvDbEntities();   
+        MvcCvDbEntities db = new MvcCvDbEntities();
         public ActionResult Index()
         {
             var hakkimda = db.TblHakkimda.ToList();
             return View(hakkimda);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyim = db.TblDeneyim.ToList();
+            return PartialView(deneyim);
+        }
+        public ActionResult Egitim()
+        {
+            var egitim = db.TblEgitim.ToList();
+            return PartialView(egitim);
+        }
+
+        public PartialViewResult Yetenek()
+        {
+            var yetenek = db.TblYetenek.ToList();
+            return PartialView(yetenek);
+        }
+
+        public PartialViewResult Hobi()
+        {
+            var hobi = db.TblHobi.ToList();
+            return PartialView(hobi);
+        }
+
+        public PartialViewResult Sertifika()
+        {
+            var sertifika = db.TblSertifika.ToList();
+            return PartialView(sertifika);
+        }
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult iletisim(Tbliletisim i)
+        {
+            i.iletisimTarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Tbliletisim.Add(i);
+            db.SaveChanges();
+            return PartialView();
         }
     }
 }
